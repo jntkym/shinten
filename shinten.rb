c138ffd407f1b8ef4eb4f9566537ce28ff1443b2
小山純汰 <junta@mmi.local>
2015-11-29 15:11:35 +0900

    modified

--- shinten.rb
+++ shinten.rb
@@ -2,53 +2,53 @@ slc = 0
 print "\n進展表並び替えスクリプト ver 0.9\n"
 print "\nEnterで続行, qで終了\n"
 while slc!=""
-	print ": "
-	slc = gets.chop
-	if slc=="q"
-		exit
-	end
+  print ": "
+  slc = gets.chop
+  if slc=="q"
+    exit
+  end
 end
 
 rslct = 0
 while rslct!="y"
-	i = 0
-	key = 0
-	s1 = []
-	s2 = []
-	s3 = []
+  i = 0
+  key = 0
+  s1 = []
+  s2 = []
+  s3 = []
 
-	print "\n生徒を入力してください(生徒がいない場合は空白)\n"
+  print "\n生徒を入力してください(生徒がいない場合は空白)\n"
   print "終了する場合はCtrl-c\n"
-	print "\n1時間目\n"
-	while i < 6
-		print ": "
-		key = gets.chop
-		s1[i] = key
-		i += 1
-	end
+  print "\n1時間目\n"
+  while i < 6
+    print ": "
+    key = gets.chop
+    s1[i] = key
+    i += 1
+  end
 
-	i = 0
-	print "2時間目\n"
-	while i < 6
-		print ": "
-		key = gets.chop
-		s2[i] = key
-		i += 1
-	end
+  i = 0
+  print "2時間目\n"
+  while i < 6
+    print ": "
+    key = gets.chop
+    s2[i] = key
+    i += 1
+  end
 
-	i = 0
-	print "3時間目\n"
-	while i < 6
-		print ": "
-		key = gets.chop
-		s3[i] = key
-		i += 1
-	end
+  i = 0
+  print "3時間目\n"
+  while i < 6
+    print ": "
+    key = gets.chop
+    s3[i] = key
+    i += 1
+  end
 
-	print "1時間目: ", s1, "\n", "2時間目: ", s2, "\n", "3時間目: ", s3, "\n"
-	print "\nこれでよろしいですか? (y/n)\n"
-	print ": "
-	rslct = gets.chomp
+  print "1時間目: ", s1, "\n", "2時間目: ", s2, "\n", "3時間目: ", s3, "\n"
+  print "\nこれでよろしいですか? (y/n)\n"
+  print ": "
+  rslct = gets.chomp
 end
 
 fst = []
@@ -59,21 +59,21 @@ trd = []
 t_id = 0
 
 for i in 0..6
-	for j in 0..6
-		if s2[i]==s3[j]
-			trd[t_id] = s2[i]
-			t_id += 1
-		end
-	end
+  for j in 0..6
+    if s2[i]==s3[j]
+      trd[t_id] = s2[i]
+      t_id += 1
+    end
+  end
 end
 
 for i in 0..6
-	for j in 0..6
-		if s1[i]==s2[j]
-			snd[s_id] = s1[i]
-			s_id += 1
-		end
-	end
+  for j in 0..6
+    if s1[i]==s2[j]
+      snd[s_id] = s1[i]
+      s_id += 1
+    end
+  end
 end
 
 fst = s1
@@ -83,21 +83,21 @@ s2_dif = Marshal.load(Marshal.dump(s2))
 s3_dif = Marshal.load(Marshal.dump(s3))
 
 for i in 0..6
-	for j in 0..6
-		if trd[i]==snd[j]
-			snd[j]=""
-		elsif trd[i]==fst[j]
-			fst[j]=""
-		end
-	end
+  for j in 0..6
+    if trd[i]==snd[j]
+      snd[j]=""
+    elsif trd[i]==fst[j]
+      fst[j]=""
+    end
+  end
 end
 
 for i in 0..6
-	for j in 0..6
-		if snd[i]==fst[j]
-			fst[j]=""
-		end
-	end
+  for j in 0..6
+    if snd[i]==fst[j]
+      fst[j]=""
+    end
+  end
 end
 
 #print fst, " ", snd, " ", trd, "\n"
