7785c205e78329cfb1ed83bbecca57398ece7911
小山純汰 <junta@mms.local>
2015-12-15 00:18:39 +0900

    add scrutinizer

--- shinten.py
+++ shinten.py
@@ -10,22 +10,45 @@ def makeTimeTable():
     print("%d 時間目" % (int(i)+1))
     print("Enter the name of student(Put blank for no student)")
     while j < 6:
-      #カンマで自動分割するようにする
+      #todo: カンマで自動分割するようにする
       print(":")
       input_name = raw_input()
       tmp_table.append(input_name)
       j = j + 1
-    time_table.append(tmp_table)
+    timetable.append(tmp_table)
     i = i + 1
   print(timetable)
   return timetable
   
-#def scrutinizeTimeTable(timetable): 
-  
+def scrutinizeTimeTable(timetable):  
+  result = []
+  for i in xrange(2):
+    dub = []
+    for j in timetable[i]:
+      for k in timetable[i+1]:
+          if k == j:
+            dub.append(k)
+            timetable[i].pop(timetable[i].index(k))
+    print "generation %d (original)" % i
+    print dub
+    result.append(timetable[i])
+    if i == 0:
+      for j in dub:
+        if j in timetable[2]:
+          dub.pop(dub.index(j))
+    print "generation %d (modified)" % i
+    print dub
+    for l in dub:
+      if l in timetable[i+1]:
+        timetable[i+1].pop(timetable[i+1].index(l))
+    result.append(dub)
+  result.append(timetable[i+1])
+  return result
+
 def main():
   time_table = makeTimeTable()
-  #result = scrutinizeTimeTable(time_table)
-  #print result
+  res_timetable = scrutinizeTimeTable(time_table)
+  print res_timetable
 
 if __name__ == '__main__':
   main()
