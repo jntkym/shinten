a7544baed6430d5be57fac38c327192946fdfebf
jntkym <ru6r1ckcu6e@gmail.com>
2015-12-15 13:48:22 +0900

    modified

--- shinten.py
+++ shinten.py
@@ -13,7 +13,7 @@ def makeTimeTable():
     name_array = name_string.split(",")
     timetable.append(name_array)
     i = i + 1
-  print(timetable)
+  print timetable
   return timetable
   
 def scrutinizeTimeTable(timetable):  
@@ -25,21 +25,16 @@ def scrutinizeTimeTable(timetable):
           if k == j:
             dub.append(k)
             timetable[i].pop(timetable[i].index(k))
-    print "generation %d (original)" % i
-    print dub
     result.append(timetable[i])
     if i == 0:
       for j in dub:
         if j in timetable[2]:
           dub.pop(dub.index(j))
-    print "generation %d (modified)" % i
-    print dub
     for l in dub:
       if l in timetable[i+1]:
         timetable[i+1].pop(timetable[i+1].index(l))
     result.append(dub)
   result.append(timetable[i+1])
-  
   return [flatten for inner in result for flatten in inner]
 
 def main():
